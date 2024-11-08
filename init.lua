9a2348bc417de2043d7a7e1a7ee580ee0ba5658c
balkin <chensy98@hotmail.com>
2024-11-08 16:15:42 +0800

    change nvim theme

--- init.lua
+++ init.lua
@@ -755,12 +755,12 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'rebelot/kanagawa.nvim',
-    name = 'kanagawa',
+    'folke/tokyonight.nvim',
+    name = 'tokyonight',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
-      vim.cmd.colorscheme 'kanagawa-wave'
+      vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
