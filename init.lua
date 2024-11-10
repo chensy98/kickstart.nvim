3fd01b4c768a296e869232dfd67f8b8c3e6de9eb
balkin <chensy98@hotmail.com>
2024-11-10 09:07:45 +0800

    change nvim them

--- init.lua
+++ init.lua
@@ -755,12 +755,12 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    name = 'tokyonight',
+    'EdenEast/nightfox.nvim',
+    name = 'nightfox',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'carbonfox'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
